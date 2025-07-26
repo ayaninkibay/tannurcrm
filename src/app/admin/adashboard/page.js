0266c56ac91dffa15f83751d2c1f1d7ceca94150
ayaninkibay <ayaninkibay@gmail.com>
2025-07-26 11:16:31 +0500

    описание

--- src/app/admin/adashboard/page.js
+++ src/app/admin/adashboard/page.js
@@ -1,7 +1,7 @@
 export default function AdminDashboard() {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Админский Dashboard</h1>
+      <h1 className="text-2xl font-bold">Админский q Dashboard</h1>
       {/* Контент админки */}
     </div>
   );
