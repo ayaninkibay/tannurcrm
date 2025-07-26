d86438b413103ae8401d7219fc815ae7bdd6a024
ayaninkibay <ayaninkibay@gmail.com>
2025-07-26 11:42:52 +0500

    Last Обновление

--- src/app/admin/adashboard/page.js
+++ src/app/admin/adashboard/page.js
@@ -1,4 +1,4 @@
-export default function AdminDashboard() {
+export default function admindashboard() {
   return (
     <div>
       <h1 className="text-2xl font-bold">Админский q Dashboard</h1>
--- src/app/admin/ateamcontent/page.js
+++ src/app/admin/ateamcontent/page.js
@@ -1,4 +1,4 @@
 import ATeamContent from './ATeamContent'
 export default function Page() {
-  return <ATeamContent />
+  return <ATeamContent/>
 }
