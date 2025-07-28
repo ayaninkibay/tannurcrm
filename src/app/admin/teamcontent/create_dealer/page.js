1ace0134a095c0d4072170cb7d5bfe565b83e215
ayaninkibay <ayaninkibay@gmail.com>
2025-07-28 17:51:10 +0500

    productview

--- src/app/admin/teamcontent/create_dealer/page.js
+++ src/app/admin/teamcontent/create_dealer/page.js
@@ -1,1 +1,9 @@
-//page
+'use client';
+
+export default function CreateDealerPage() {
+  return (
+    <div>
+      <h1>Create Dealer Page</h1>
+    </div>
+  );
+}
--- src/app/admin/teamcontent/create_star/page.js
+++ src/app/admin/teamcontent/create_star/page.js
@@ -1,1 +1,9 @@
-//page
+'use client';
+
+export default function CreateStarPage() {
+  return (
+    <div>
+      <h1>Create Star Page</h1>
+    </div>
+  );
+}
